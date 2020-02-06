aaa007302cca9379d746aba7c2b515a15dec01c8
Raul Cesar Mulerschat <raulzito737@gmail.com>
2020-02-06 06:44:36 -0300

    array de objetos

--- src/cursojava/executavel/ArrayVetor.java
+++ src/cursojava/executavel/ArrayVetor.java
@@ -4,12 +4,10 @@ import cursojava.classes.Aluno;
 import cursojava.classes.Disciplina;
 
 public class ArrayVetor {
-    public static  void main(String[] args) {
+    public static void main(String[] args) {
 
-       
-
-        Double[] notas= {8.8,9.9,7.7,6.6};
-        Double[] notasLOGICA= {6.8,4.9,7.4,5.6,5.9,8.9,8.7};
+        Double[] notas = { 8.8, 9.9, 7.7, 3.1 };
+        Double[] notasLOGICA = { 6.8, 4.9, 7.4, 5.6, 10.0, 5.9, 9.8 };
 
         Aluno aluno = new Aluno();
         aluno.setNome("RAUL CESAR ");
@@ -20,24 +18,31 @@ public class ArrayVetor {
         disciplina.setNota(notas);
         aluno.getDisciplinas().add(disciplina);
 
-        
         Disciplina disciplina2 = new Disciplina();
         disciplina2.setDisciplina("lOGICA DE PROGRAMAÇÃO");
         disciplina2.setNota(notasLOGICA);
         aluno.getDisciplinas().add(disciplina2);
-        
-        System.out.println("NOME DO ALUNO :"+aluno.getNome()+"\n"+"NOME DO CURSO  :"+aluno.getNomeEscola());
-      for ( Disciplina d :  aluno.getDisciplinas()) {
-          System.out.println("*************DISCIPLINA**************");
-         System.out.println(d.getDisciplina());
-         System.out.println("*****************NOTAS*****************");
-         for (int i = 0; i <d.getNota().length; i++) {
-             System.out.println("NOTA "+(i+1)+ " :      "+d.getNota()[i]);///PARA O USO DOS [] NÃO PRESICA DE NOTAÇÃO PONTO
-             
-         }
-           
-      }
 
+        // ********************************************************* */
+        Aluno[] arrayAlunos = new Aluno[1];
+
+        arrayAlunos[0] = aluno;
+
+        for (int i = 0; i < arrayAlunos.length; i++) {
+            //// array aluno
+            System.out.println("nome =" + arrayAlunos[i].getNome());
+
+            //// array de disciplina
+            for (Disciplina d : arrayAlunos[i].getDisciplinas()) {
+                System.out.println("disciplina " + d.getDisciplina());
+
+                // array de notas
+
+                for (int j = 0; j < d.getNota().length; j++) {
+                    System.out.println("notas" + d.getNota()[j]);
+                }
+            }
+        }
 
     }
 
